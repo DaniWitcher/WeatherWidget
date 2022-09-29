ca80947c047bb81dfe596afeb47fbfb3410a2fc7
DaniWitcher <accforjavarush@gmail.com>
2022-09-29 18:48:19 +0400

    Added Weekly and dayly weather classes

---
+++ DaylyWeather.js
@@ -0,0 +1,12 @@
+class DaylyWeather{
+
+    constructor(id, dayName, date, temperature, humidity, weather)
+    {
+        this.id = id;
+        this.dayName = dayName;
+        this.date = date;
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.weather = weather;
+    }  
+}
